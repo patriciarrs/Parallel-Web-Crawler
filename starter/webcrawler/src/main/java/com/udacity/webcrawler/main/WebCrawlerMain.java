c7a554158a79e78eb5f659f5002e542496ad85da
Patrícia Silva <prrs89@gmail.com>
2025-12-22 17:21:36 +0000

    Step 3. Running the Legacy Crawler

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -14,39 +14,53 @@ import javax.inject.Inject;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Objects;
 
 public final class WebCrawlerMain {
 
-  private final CrawlerConfiguration config;
+    private final CrawlerConfiguration config;
+    @Inject
+    private WebCrawler crawler;
+    @Inject
+    private Profiler profiler;
 
-  private WebCrawlerMain(CrawlerConfiguration config) {
-    this.config = Objects.requireNonNull(config);
-  }
-
-  @Inject
-  private WebCrawler crawler;
-
-  @Inject
-  private Profiler profiler;
-
-  private void run() throws Exception {
-    Guice.createInjector(new WebCrawlerModule(config), new ProfilerModule()).injectMembers(this);
-
-    CrawlResult result = crawler.crawl(config.getStartPages());
-    CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
-  }
-
-  public static void main(String[] args) throws Exception {
-    if (args.length != 1) {
-      System.out.println("Usage: WebCrawlerMain [starting-url]");
-      return;
+    private WebCrawlerMain(CrawlerConfiguration config) {
+        this.config = Objects.requireNonNull(config);
     }
 
-    CrawlerConfiguration config = new ConfigurationLoader(Path.of(args[0])).load();
-    new WebCrawlerMain(config).run();
-  }
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: WebCrawlerMain [starting-url]");
+            return;
+        }
+
+        CrawlerConfiguration config = new ConfigurationLoader(Path.of(args[0])).load();
+        new WebCrawlerMain(config).run();
+    }
+
+    private void run() throws Exception {
+        Guice.createInjector(new WebCrawlerModule(config), new ProfilerModule()).injectMembers(this);
+
+        CrawlResult result = crawler.crawl(config.getStartPages());
+        CrawlResultWriter resultWriter = new CrawlResultWriter(result);
+
+        // Write the crawl results to a JSON file (or System.out if the file name is empty)
+        String resultOutputPath = config.getResultPath();
+
+        // If resultOutputPath is empty, write to System.out
+        if (resultOutputPath.isEmpty()) {
+            // OutputStreamWriter converts an OutputStream (like System.out that is a PrintStream) into a Writer
+            Writer writer = new OutputStreamWriter(System.out, StandardCharsets.UTF_8);
+            resultWriter.write(writer);
+            writer.close();
+        } else {
+            // Write the crawl results to a JSON file
+            Path path = Path.of(resultOutputPath);
+            resultWriter.write(path);
+        }
+
+        // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+    }
 }
